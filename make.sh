d8697a75ed053a2cc804c1f633fb6b64d8b56b11
40km/h <132439355+hentai-binarian@users.noreply.github.com>
2025-07-28 22:13:52 +0900

    Add files via upload

---
+++ make.sh
@@ -0,0 +1,10 @@
+nasm -f bin boot.s -o boot.bin
+nasm -f elf32 kernel.s -o kernel.o
+nasm -f elf32 lib.s -o lib.o
+
+ld -m elf_i386 -Ttext 0x7E00 -o kernel.elf kernel.o lib.o
+
+objcopy -O binary kernel.elf kernel.bin
+
+cat boot.bin kernel.bin > os.img
+
